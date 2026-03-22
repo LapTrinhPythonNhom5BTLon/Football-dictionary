439e6784c77632956f5a294ad01462e914dd21e5
Khuong <killer123578910@gmail.com>
2026-03-22 10:14:26 +0700

    sele

--- laydata.py
+++ laydata.py
@@ -1,54 +1,25 @@
 from selenium import webdriver
 from selenium.webdriver.edge.service import Service
-from selenium.webdriver.edge.options import Options
+from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from bs4 import BeautifulSoup, Comment
 
 service = Service(r"D:\sieurac\edgedriver_win64\msedgedriver.exe")
+driver = webdriver.Edge(service=service)
 
-options = Options()
-options.add_argument(r"user-data-dir=D:\edge_profile_test")
-options.add_argument("--disable-blink-features=AutomationControlled")
+driver.get("https://youtube.com")
 
-driver = webdriver.Edge(service=service, options=options)
-
-driver.get("https://fbref.com/en/comps/9/2023-2024/stats/2023-2024-Premier-League-Stats")
-
-print("⏳ Đợi Cloudflare verify...")
-
-# 🔥 CHỜ khi element thật xuất hiện
-WebDriverWait(driver, 30).until(
-    EC.presence_of_element_located((By.TAG_NAME, "body"))
-)
-
-# ⚠️ check thêm: phải KHÔNG còn chữ "Just a moment"
-WebDriverWait(driver, 30).until_not(
-    EC.text_to_be_present_in_element((By.TAG_NAME, "body"), "Just a moment")
-)
-
-print("✅ Đã vào trang thật")
-
-html = driver.page_source
-
-# ===== parse =====
-soup = BeautifulSoup(html, "html.parser")
-
-comments = soup.find_all(string=lambda text: isinstance(text, Comment))
-
-table = None
-
-for c in comments:
-    if "stats_standard" in c:
-        soup2 = BeautifulSoup(c, "html.parser")
-        table = soup2.find("table_container is_setup")
-        break
-
-if table:
-    print("✅ Lấy được bảng!")
-    print(table.text[:500])
-else:
-    print("❌ Vẫn chưa có bảng")
-
-driver.quit()
+search=driver.find_element(By.NAME,"search_query")
+search.send_keys("zweihander")
+search.send_keys(Keys.RETURN)
+try:
+    textt=WebDriverWait(driver,10).until(
+        EC.presence_of_element_located((By.CLASS_NAME,"style-scope ytd-search"))
+    )
+    textt=driver.find_element(By.CLASS_NAME,"style-scope ytd-search")
+    print(textt.text)
+except:
+    driver.quit()
+input()

040631d68f088c2f53d5de62a66254b6a80c0916
dimm <ducanh55567@gmail.com>
2026-04-06 14:38:51 +0700

    asd

--- priceoffootballer.py
+++ priceoffootballer.py
@@ -1,5 +1,4 @@
 import sqlite3
-
 from seleniumbase import SB
 
 from selenium.webdriver.common.by import By
@@ -17,57 +16,47 @@ curs=conn.cursor()
 curs.execute("SELECT player FROM players")
 
 rows=curs.fetchall()
-curs.execute("DROP TABLE IF EXIST prices")
 
 curs.execute("""
-
     CREATE TABLE IF NOT EXISTS prices (
-
         player TEXT,
-
         price TEXT
-
     )
+""")
 
-    """)
-
+curs.execute("SELECT player FROM players")
+rows = curs.fetchall()
 conn.commit()
 
-
-
-
-
 with SB(uc=True) as sb:
-
     sb.open("https://www.footballtransfers.com/en/search")
-
+    
     for row in rows:
-
-        sb.type('input[name="search_value"]', f"{str(*row)}\n")
-        pri=''
+        player_name = str(row[0]).strip()
+        print(f"Đang tìm giá cho: {player_name}")
+        
+        sb.type('input[name="search_value"]', f"{player_name}\n")
+        
+        pri = 'N/A'
         try:
-            sb.wait_for_element(f'a[title="{str(*row)}"]', timeout=7)
+            player_link_selector = f'a[title*="{player_name}"]'
+            
+            sb.wait_for_element(player_link_selector, timeout=7)
+            
+            price_selector = f'{player_link_selector} .player-price .player-tag'
+            
+            if sb.is_element_present(price_selector):
+                pri = sb.get_text(price_selector).strip()
+            else:
+                pri = sb.get_text(f'{player_link_selector} .player-price').strip()
+                
+        except Exception:
+            pri = 'N/A'
+        
+        print(f"-> Kết quả: {pri}")
 
-            html=sb.get_page_source()
-
-            soup=BeautifulSoup(html,"html.parser")
-
-            cauthu=soup.find("a",{"title":str(*row)})
-            pri=cauthu.find("div",{"class":"player-price"}).find("span",{"class":"player-tag"}).text
-        except:
-            pri='N/A'
-        curs.execute("""
-
-            INSERT INTO prices VALUES (?,?)
-
-            """, (
-
-                str(*row),
-
-                pri
-
-            ))
-
+        curs.execute("INSERT INTO prices (player, price) VALUES (?, ?)", (player_name, pri))
         conn.commit()
 
-    conn.close()
+conn.close()
+print("Hoàn thành!")

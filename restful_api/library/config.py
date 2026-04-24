fa5e58bf6f83fd5c471113b4940d208b8704dd62
NgMinhDucc <ducnm@gdscptit.dev>
2026-04-24 22:44:37 +0700

    deleted

--- restful_api/library/config.py
+++
@@ -1,7 +0,0 @@
-import os
-from dotenv import load_dotenv
-
-load_dotenv() # chuyen du lieu tho trong .env -> py
-SECRET_KEY = os.environ.get("KEY")
-DB_URL = os.environ.get("DATABASE_URL")
-TRACK_MODIFICATIONS = False

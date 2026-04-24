3e02a58083d4512ed8bc053b3789897962e6492b
NgMinhDucc <ducnm@gdscptit.dev>
2026-04-24 22:44:09 +0700

    updated

--- restful_api/library/__init__.py
+++ restful_api/library/__init__.py
@@ -2,9 +2,8 @@ from flask import Flask
 from .players.controller import player_name as pn
 from .team.controller import team_name as tn
 
-def create_app(config_file = "config.py"):
+def create_app():
     app = Flask(__name__)
-    app.config.from_pyfile(config_file)
     app.json.sort_keys = False
     
     app.register_blueprint(pn)

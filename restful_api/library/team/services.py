96d27ae30ea60f666d3f1f1c1fd01eff2870a95d
NgMinhDucc <ducnm@gdscptit.dev>
2026-04-27 13:36:57 +0700

    fixed path of db

--- restful_api/library/team/services.py
+++ restful_api/library/team/services.py
@@ -3,7 +3,7 @@ import sqlite3
 
 def get_team_data(name):
     cur_dir = os.path.dirname(os.path.abspath(__file__))
-    db_path = os.path.abspath(os.path.join(cur_dir, "../../players.db"))
+    db_path = os.path.abspath(os.path.join(cur_dir, "../../../players.db"))
     
     conn = sqlite3.connect(db_path)
     
